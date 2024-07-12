ad20af0df8701d62f8365c735ab99919314b65fe
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-12 14:11:44 +0100

    Config: development proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080
+    port: 8080,
+    proxy: {
+      '/products': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
 })
