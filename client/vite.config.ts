5cb965080dd501527b632b48e6c4888a20675009
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-12 17:03:15 +0100

    Update API path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 8080,
     proxy: {
-      '/products': {
+      '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
